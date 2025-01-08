6bba039cafdd68b2100243de22ada5e3a111ddf0
Dekopossas <andrepossaster@gmail.com>
2025-01-08 15:52:16 -0300

    fix routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,9 +13,9 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cors());
 
-app.use("/", flightsRoute); // Correctly scoped to API
-app.use("/api", testRoute); // Scoped to other API requests
-// app.use("/", (_req, res) => res.status(200).send("Rodando Aplicação na 3001"));
+app.use("/api/flights", flightsRoute); // Correctly scoped to API
+// app.use("/api", testRoute); // Scoped to other API requests
+app.use("/", (_req, res) => res.status(200).send("Rodando Aplicação na 3001"));
 
 app.use(errorMiddleware);
 
